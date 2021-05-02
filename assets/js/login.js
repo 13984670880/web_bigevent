c6865377b4f156311e7a02ac3fdd940a30637030
杨胜军 <1678146276@qq.com>
2021-05-02 11:46:18 +0800

    完成个人中心功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,23 +16,23 @@ $(function() {
     var layer = layui.layer;
     // 通过 form.verify() 函数自定义校验规则
     form.verify({
-        // 自定义了一个叫做 pwd 校验规则
-        pwd: [
-            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ],
-        // 校验两次密码是否一致的规则
-        repwd: function(value) {
-            // 通过形参拿到的是确认密码框的内容
-            // 还需要拿到密码框的内容
-            // 然后进行一次等于的判断
-            // 如果判断失败，则return一个提示的消息框
-            var pwd = $('.reg-box [name=password]').val();
-            if (pwd !== value) {
-                return '两次密码不一致！';
+            // 自定义了一个叫做 pwd 校验规则
+            pwd: [
+                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+            ],
+            // 校验两次密码是否一致的规则
+            repwd: function(value) {
+                // 通过形参拿到的是确认密码框的内容
+                // 还需要拿到密码框的内容
+                // 然后进行一次等于的判断
+                // 如果判断失败，则return一个提示的消息框
+                var pwd = $('.reg-box [name=password]').val();
+                if (pwd !== value) {
+                    return '两次密码不一致！';
+                }
             }
-        }
-    });
-    // 监听注册表单的提交事件
+        })
+        // 监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
         // 阻止表单默认提事件
         e.preventDefault();
